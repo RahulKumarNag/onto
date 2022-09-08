364b1135a69c12d2e65794ffc0b5f6c95f299c37
RahulkumarNag <rahul4321nag@gmail.com>
2022-09-09 01:54:16 +0530

    email

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $name = trim($_POST['contact-name']);
 $email = trim($_POST['contact-email']);
 $company = trim($_POST['contact-company']);
-$message = trim($_POST['contact-message']);
+// $message = trim($_POST['contact-message']);
 if ($name == "") {
     $msg['err'] = "\n Name can not be empty!";
     $msg['field'] = "contact-name";
@@ -19,25 +19,27 @@ if ($name == "") {
     $msg['err'] = "\n Please put a valid email address!";
     $msg['field'] = "contact-email";
     $msg['code'] = FALSE;
-} else if ($message == "") {
-    $msg['err'] = "\n Message can not be empty!";
-    $msg['field'] = "contact-message";
-    $msg['code'] = FALSE;
-} else {
-    $to = 'hasan@axilweb.com';
-    $subject = 'Abstrak Contact Query';
+}
+// } else if ($message == "") {
+//     $msg['err'] = "\n Message can not be empty!";
+//     $msg['field'] = "contact-message";
+//     $msg['code'] = FALSE;
+// } 
+  else {
+    $to = 'contact@ontoclassroom.com';
+    $subject = 'Ontoclassroom Contact Query';
     $_message = '<html><head></head><body>';
     $_message .= '<p>Name: ' . $name . '</p>';
     $_message .= '<p>Email: ' . $email . '</p>';
     $_message .= '<p>Company: ' . $company . '</p>';
-    $_message .= '<p>Message: ' . $message . '</p>';
+    // $_message .= '<p>Message: ' . $message . '</p>';
     $_message .= '</body></html>';
 
     $headers = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= 'From:  Abstrak <contact@example.com>' . "\r\n";
-    $headers .= 'cc: contact@example.com' . "\r\n";
-    $headers .= 'bcc: contact@example.com' . "\r\n";
+    $headers .= 'From:  Onto <contact@example.com>' . "\r\n";
+    $headers .= 'cc: ontoclassroom@gmail.com' . "\r\n";
+    $headers .= 'bcc: rahul4321nag@gmail.com' . "\r\n";
     mail($to, $subject, $_message, $headers, '-f contact@example.com');
 
     $msg['success'] = "\n Email has been sent successfully.";
